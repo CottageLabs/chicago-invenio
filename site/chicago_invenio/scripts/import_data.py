15236d747e70e81eb7c93a6fc43f9dd39beac5ad
Hrafn Malmquist <hrafn.malmquist@gmail.com>
2026-01-28 14:42:30 +0000

    Comment out ISSN handling

--- site/chicago_invenio/scripts/import_data.py
+++ site/chicago_invenio/scripts/import_data.py
@@ -957,16 +957,16 @@ def parse_marc_record(record_elem, record_identifier) -> Tuple[Dict[str, Any], L
                 })
 
     # ISSN (MARC 022$a)
-    issn_fields = record_elem.findall('.//marc:datafield[@tag="022"]', MARC_NS)
-    for issn_field in issn_fields:
-        issn_a = issn_field.find('.//marc:subfield[@code="a"]', MARC_NS)
-        if issn_a is not None:
-            issn_value = issn_a.text.strip()
-            if issn_value and not any(existing['identifier'] == issn_value for existing in identifiers):
-                identifiers.append({
-                    'identifier': issn_value,
-                    'scheme': 'issn'
-                })
+    #issn_fields = record_elem.findall('.//marc:datafield[@tag="022"]', MARC_NS)
+    #for issn_field in issn_fields:
+    #    issn_a = issn_field.find('.//marc:subfield[@code="a"]', MARC_NS)
+    #    if issn_a is not None:
+    #        issn_value = issn_a.text.strip()
+    #        if issn_value and not any(existing['identifier'] == issn_value for existing in identifiers):
+    #            identifiers.append({
+    #                'identifier': issn_value,
+    #                'scheme': 'issn'
+    #            })
 
     # Electronic location/URLs (MARC 856)
     url_fields = record_elem.findall('.//marc:datafield[@tag="856"]', MARC_NS)
